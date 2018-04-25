08f0cdc3d97ff5489d123be670e5b7b185c5578a
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-25 22:44:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,8 +15,7 @@ def position_taken?(arr, ind)
 end
 
 def valid_move?(arr, ind)
-  # ( ind != -1 && ind.between?(0, 8) && position_taken?(arr, ind) ) ? true : false
-  ( ind != -1 && ind.between?(0, 8) && position_taken?(arr, ind) ) ? true : false
+  ind != -1 && ind.between?(0, 8) && position_taken?(arr, ind)
 end
 
 def move(arr, ind, char="X")
